fb049942870b45ada521c70462da7ddb5fa8259a
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-04-21 23:25:39 +0200

    tentative d ajout d indice

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const list = document.querySelector('#results');
+const indiceEvent = document.querySelector('.indice');
 let pokeName = "";
 let score = 0;
+
+const fetchPokemon = (query) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon-species/${query}`)
+    .then(response => response.json())
+    .then(insertPokemon)
+    .then(insertPokemonName);
+  };
 
 const insertPokemon = (data) => {
       const pokeTag = `<div class="info">
       <p class="score">${score}</p>
       </div>
       <div class="text-center">
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="Pokemon image" />
+      <img class="pokeImage" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="Pokemon image" />
       </div>`;
     list.insertAdjacentHTML('beforeend', pokeTag);
     if(data.names[3].language.name === "fr") {
@@ -18,22 +26,28 @@ const insertPokemon = (data) => {
 }
 
 const insertPokemonName = (data) => {
-      const len = pokeName.length
-      pokeHide = "_ ".repeat(len);
-      const pokName = `<div class="info">
-      <p class="pokehide text-center w-100">${pokeHide}(${(len)})</p>
-      </div>`;
-    list.insertAdjacentHTML('beforeend', pokName);
-    document.title = `${pokeHide} - Peux-tu le trouver ?`;
+  const len = pokeName.length
+  pokeHide = "_ ".repeat(len);
+  const pokName = `<div class="info">
+    <p class="pokehide text-center w-100">${pokeHide}(${(len)})</p>
+    </div>`;
+  list.insertAdjacentHTML('beforeend', pokName);
+  document.title = `${pokeHide} - Peux-tu le trouver ?`;
 }
 
 
-const fetchPokemon = (query) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon-species/${query}`)
-    .then(response => response.json())
-    .then(insertPokemon)
-    .then(insertPokemonName);
-};
+// const indice = () => {
+//   const displayName = document.querySelector('.pokehide');
+//   const len = pokeName.length
+//   pokeHid = (pokeName[0] + "_ ".repeat(len-2) + pokeName[len-1]+ `(${len})`).toUpperCase();
+//   displayName.innerHTML = pokeHid;
+//   // list.insertAdjacentHTML('beforeend', pokName);
+//   document.title = `${pokeHid} - Peux-tu le trouver ?`;
+// }
+//
+// indiceEvent.addEventListener("click", indice());
+
+
 
 window.addEventListener("load", fetchPokemon(Math.floor(Math.random() * 898)));
 // fetchPokemon(Math.floor(Math.random() * 898)); // on 1st page load
